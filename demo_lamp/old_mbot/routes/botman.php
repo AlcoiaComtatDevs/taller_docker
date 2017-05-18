9fcf914662b97024261b0a329fb2b641393a85b9
Jose Ruzafa Sierra <jose.ruzafa@gmail.com>
2017-05-18 18:28:24 +0200

    remove meanjs, add lamp base

--- demo_lamp/old_mbot/routes/botman.php
+++
@@ -1,9 +0,0 @@
-<?php
-use App\Http\Controllers\BotManController;
-// Don't use the Facade in here to support the RTM API too :)
-$botman = resolve('botman');
-
-$botman->hears('test', function($bot){
-    $bot->reply('hello! panda');
-});
-$botman->hears('Start', BotManController::class.'@startConversation');
